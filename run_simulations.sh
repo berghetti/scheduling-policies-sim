028036fb272b6a93efc03d40ee82688a77919e0e
Mayco <mayco.s.berghetti@gmail.com>
2023-01-26 08:19:21 -0400

    new results bimodal_flag and constant flag

--- run_simulations.sh
+++ run_simulations.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
 CONF_FILES=(
-    "./configs/constant_dfcfs.json"
-    "./configs/constant_ec_static_config.json"
+    "./configs/bimodal_flag_static_config.json"
     "./configs/constant_flag_static_config.json"
-    "./configs/constant_new_policy.json"
-    "./configs/constant_ws_static_config.json"
 )
 
 DESCRIPTIONS=(
-    "constant_dfcfs"
-    "constant_ec"
+    "bimodal_flag"
     "constant_flag"
-    "constant_new_policy"
-    "constant_ws"
 )
 
 for i in ${!CONF_FILES[@]}; do
     echo "Starting: ${DESCRIPTIONS[$i]}"
     ./start.sh ${CONF_FILES[$i]} ${DESCRIPTIONS[$i]}
 done
+
+git add . && git commit -m "new results bimodal_flag and constant flag" && git push && shutdown -P
