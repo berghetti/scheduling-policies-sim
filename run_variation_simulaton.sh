7349bcb6744369412c8f9a3a4cfeb93a37a7b6eb
Mayco <mayco.s.berghetti@gmail.com>
2023-01-26 13:46:30 -0400

    new test variation load

---
+++ run_variation_simulaton.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Run one simulation
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage $0 config.json description"
+fi
+
+python3 ./sim/run_sim.py $1 $2
+
+
+FILE=$(tail -n 1 ./results/meta_log | awk -F': ' '{print $1}')
+FILE_OUT="${FILE}_$2_out.csv"
+python3 analysis.py $FILE $FILE_OUT 0
+
+awk -F, '{print($11 "," $9 "," $12 "," $10)}' $FILE_OUT | column --separator , -t
+
+
