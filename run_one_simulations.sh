bfa4592d2bf8e5ca901f914519762907a7d57f64
Mayco <mayco.s.berghetti@gmail.com>
2023-10-07 20:49:34 -0400

    minor changes

--- run_one_simulations.sh
+++ run_one_simulations.sh
@@ -6,7 +6,7 @@ if [ "$#" -ne 2 ]; then
     echo "Usage $0 config.json description"
 fi
 
-#python3 ./sim/simulation.py $1 $2 $3
+python3 ./sim/simulation.py $1 $2 $3
 
 
 FILE=$(tail -n 1 ./results/meta_log | awk -F': ' '{print $1}')
@@ -14,6 +14,6 @@ FILE_OUT="${FILE}_$2_out.csv"
 echo $FILE $FILE_OUT
 python3 analysis.py $FILE $FILE_OUT 0
 
-awk -F, '{print($11 "," $9 "," $12 "," $10 "," $13 "," $14 "," $19 "," $20 "," $21)}' $FILE_OUT | column --separator , -t
+awk -F, '{print($12 "," $13 "," $11 "," $16 "," $17 "," $18 "," $19 "," $22)}' $FILE_OUT | column --separator , -t
 
 
