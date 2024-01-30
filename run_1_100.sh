b4dadc21f4e766e0dce1e6b0b4006b5ed0decc13
Mayco S. Berghetti <mayco.s.berghetti@gmail.com>
2024-01-30 14:50:23 -0400

    improve threads usage

--- run_1_100.sh
+++ run_1_100.sh
@@ -139,6 +139,9 @@ set_worker_cores()
 }
 RUNS=10 # runs same test in multiple threads
 
+MAX_THREADS=140
+counter=0
+
 exec_test()
 {
     TEST_NAME=$1
@@ -160,11 +163,14 @@ exec_test()
        mv ./results/sim_${RANDOM_TEST_NAME}_t${i}/task_times.csv \
            ./tests/${DIR_TEST}/${i}_tasks.csv;
    done
+
+   ((counter--))
 }
 
 [ -d tests ] || mkdir tests
 
 LOAD_NAME="1_100"
+
 
 run_afp()
 {
@@ -186,11 +192,13 @@ run_afp()
         for load in {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9}; do
           set_avg_system_load $load $CONF
           exec_test "${dist}/${LOAD_NAME}" "afp_ov${OVERHEAD}_q$((q/1000))" $CONF $load  &
-          PID=$!
           sleep 10
+          ((counter++))
+          if [$counter -eq $MAX_THREADS]; then
+            wait -n # wait lest one process return
+          fi
         done
 
-        wait $PID
 
       done
 
@@ -230,14 +238,18 @@ run_psp()
 
       for ov in {150,300,450,600}; do
           set_psp_overhead $ov $CONF
+         
           for load in {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9}; do
              set_avg_system_load $load $CONF
              exec_test "${dist}/${LOAD_NAME}" "psp_ov${ov}_res${RESERVED}" $CONF $load &
-             PID=$!
              sleep 10
+
+             ((counter++))
+             if [$counter -eq $MAX_THREADS]; then
+               wait -n # wait lest one process return
+             fi
            done
 
-           wait $PID
       done
 
   done
